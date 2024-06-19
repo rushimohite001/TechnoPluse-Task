9a272650a42114c462b1fbb951e3ca594e867270
Rushi Mohite <rushimohite2002@gmail.com>
2024-06-19 15:40:52 +0530

    Add Billing Task

---
+++ Biling Task/script.js
@@ -0,0 +1,11 @@
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const toggleIcon = document.querySelector('.toggle-password');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.textContent = '🙈';
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.textContent = '👁️';
+    }
+}
